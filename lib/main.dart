de6da0d6b9c87158b0a5c5e33427d89379dadba5
nyanyacyan <nyanyacyan@gmail.com>
2025-04-25 08:26:40 +0900

    修正、コメント追加

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,15 @@
 // Flutterの基本UIライブラリを読み込む
+// Material DesignのUI部品を使うためのライブラリ
+// Material DesignはGoogleが提唱するUIデザインのガイドライン→ボタンなどが見やすいものを定義しているデザイン
+
+// UIパーツには2大派閥がある
+// cupertino.dart → iOS風のUI部品
+// material.dart → Android風のUI部品
+// Androidもiosの両方使う場合にはmaterial.dartを使うのが一般的
 import 'package:flutter/material.dart';
 
-// HTTP通信のためのパッケージを読み込む
-// yamlへ追加コマンド　=　flutter pub add http
+// APIリクエストするためのライブラリ
+// yamlでバージョンを指定してインストールする必要がある
 import 'package:http/http.dart' as http;
 
 // Jsonデータを扱うための標準ライブラリ
@@ -14,6 +21,7 @@ void main() {
   runApp(const MyApp());
 }
 
+// アプリのルートウィジェット
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
@@ -21,114 +29,88 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: '医療機関検索Google口コミ順位アプリ',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue),
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const SearchScreen(),
     );
   }
 }
 
+// 検索画面の箱を用意
+// extends StatefulWidgetはFlutterの基底クラス → StatefulWidgetとは
+// class SearchScreenというクラスを定義している → このクラスはStatefulWidgetを継承している
+// StatelessWidgetは一度定義したら変えられない
+// StatefulWidgetは状態を持つウィジェットで、状態が変わるとUIも変わる → ホワイトボード→何回でも書き換えられる
+class SearchScreen extends StatefulWidget {
+    // 検索画面のコンストラクタ→keyは目印的なもの→定番の書き方
+    const SearchScreen({super.key});
 
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
+    // @overrideはオーバーライドを示すキーワード
+    @override
+    // createsState() をオーバーライドさせる → 処理の中身は _SearchScreenState にオーバーライドさせる→Widgetの中身をの処理を入れ込んでいくイメージ
+    // createsStateメソッドはState<StatefulWidget>という状態が定義されている→State<SearchScreen>とすることでState<SearchScreen>を定義している呼び出し元と紐づけできる。
+    // インスタンス化もここで行われる
+    State<SearchScreen> createsState() => _SearchScreenState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
+// 検索画面の状態を管理するクラス
+// State<SearchScreen>の状態持ったクラスを定義している
+// _SearchScreenStateはプライベートなクラスで、アンダースコアをつけることで他のファイルからはアクセスできないようにしている
+class _SearchScreenState extends State<SearchScreen> {
 
-  @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text('You have pushed the button this many times:'),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
-    );
-  }
+    // finalは変更できない変数を定義するためのキーワード(変数の代入ができない)
+    // TextEditingControllerはテキストフィールドの入力を管理するためのクラス
+    // Dartでの変数は 型＋変数名 = 値 という形で定義する
+    // 変数名は小文字から始めるのが一般的でキャメルケースで定義するのが一般的
+    final TextEditingController _stationController = TextEditingController();
+
+    String _selectedType = '小児科';
+
+    // Mapは辞書データ dynamic型は全ての型を受け入れることができる
+    // _placesは医療機関の情報を格納するための変数
+    // "_"から始まる変数はプライベートな変数であることを示す→他のファイルからはアクセスできないようにしている
+    List<Map<String, dynamic>> _places = [];
+
+    // Future	「あとで結果が返ってくるよ！」という約束（予約）
+    // <void>	「何も返さない」って意味（数字とか文字は返さない）
+    // searchPlaces()	関数の名前。「検索するよ」って意味の関数名
+    // async	「時間がかかる処理だよ！」と教える魔法の言葉
+    Future<void> searchPlaces() async {
+        // .trim()	前後の空白を取り除く（スペースや改行を消してくれる）
+        final station = _stationController.text.trim();
+        if (station.isEmpty) return;
+
+        const apiKey = "YOUR_GOOGLE_API_KEY";
+
+        final geocodeUrl = "https://maps.googleapis.com/maps/api/geocode/json?address=$station&key=$apiKey";
+        final geocodeResponse = await http.get(Uri.parse(geocodeUrl));
+        final geocodeData = json.decode(geocodeResponse.body);
+
+        if (geocodeData['status'] == "OK"){
+            final lat = geocodeData['results'][0]['geometry']['location']['lat'];
+            final lng = geocodeData['results'][0]['geometry']['location']['lng'];
+
+            final typeMap = {
+                '小児科': 'doctor',
+                '内科': 'doctor',
+                '耳鼻科': 'doctor',
+                '眼科': 'doctor',
+                '皮膚科': 'doctor',
+                '整形外科': 'doctor',
+            };
+
+            final placesUrl = "https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=$lat,$lng&radius=1500&type=${typeMap[_selectedType]}&key=$apiKey";
+            final placesResponse = await http.get(Uri.parse(placesUrl));
+            final placesData = json.decode(placesResponse.body);
+
+            setState(() {
+                _places = placesData['results'] ?? [];
+            });
+        }
+    }
+
 }
