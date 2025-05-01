57c7690fda27d7963e4fbcff7cc8a63f66a8e6ff
nyanyacyan <nyanyacyan@gmail.com>
2025-05-01 21:06:44 +0900

    Androidアプリ用にaabファイルの生成ができるように修正

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -28,7 +28,8 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.example.gm_reviews_search_doctor_app"
+        // アプリケーションIDを指定
+        applicationId = "com.nyanyacyan.gmreviews"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
