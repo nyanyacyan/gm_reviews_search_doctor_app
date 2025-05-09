dead6ca95f2d18529b1c9ed1fcc8ee321d694dbc
nyanyacyan <nyanyacyan@gmail.com>
2025-05-10 04:19:25 +0900

    タイトル変更、アイコン変更

--- lib/strings.dart
+++ lib/strings.dart
@@ -2,7 +2,7 @@
 
 class AppStrings {
   // static const →どこからでも呼び出せる定数
-  static const String appTitle = '駅チカ 名医リサーチ | 口コミランキング';
+  static const String appTitle = '口コミドクター';
   static const String searchHint = '駅名を入力してください';
   static const String dropDownSelectHint = '希望の診療科を選択してください';
   static const String searchButton = '検索';
